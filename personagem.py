9b5f9113a78414313f6497f1da34a74b3eca5485
Discretinhos <laboratorialequipe@gmail.com>
2024-11-26 01:01:41 -0300

    Update personagem.py

--- personagem.py
+++ personagem.py
@@ -1,36 +1,39 @@
 import pygame
-from consts import LARGURA, ALTURA,PERSONAGEM_X, PERSONAGEM_Y, PERSONAGEM_LARGURA, PERSONAGEM_ALTURA, carregar_recursos
-
-# Carrega os recursos
-RECURSOS = carregar_recursos()
-
+from consts import *
 class Personagem:
-    def __init__(self, x=PERSONAGEM_X, y=PERSONAGEM_Y):
+    def __init__(self, x, y):  # Corrigido de _init_ para __init__
         self.x = x
         self.y = y
         self.velocidade = 5
         self.direcao = 'direita'
         self.moving = False
-        self.carregar_sprites()
 
-    def carregar_sprites(self):
-        self.sprite_parado = RECURSOS['sprites_parado']
-        self.sprite_moving1 = RECURSOS['sprites_moving1']
-        self.sprite_moving2 = RECURSOS['sprites_moving2']
+        # Carregar as imagens do personagem
+        self.sprite_parado = pygame.image.load("assets/Recursos/sprites_Ella_prota-standing.png").convert_alpha()
+        self.sprite_moving1 = pygame.image.load("assets/Recursos/sprites_Ella_prota-moving1.png").convert_alpha()
+        self.sprite_moving2 = pygame.image.load("assets/Recursos/sprites_Ella_prota-moving2.png").convert_alpha()
 
+        # Criar animações de movimento para a direita e esquerda
         self.sprites_direita = [self.sprite_moving1, self.sprite_moving2]
-        self.sprites_esquerda = [pygame.transform.flip(self.sprite_moving1, True, False),
-                                 pygame.transform.flip(self.sprite_moving2, True, False)]
+        self.sprites_esquerda = [
+            pygame.transform.flip(self.sprite_moving1, True, False),
+            pygame.transform.flip(self.sprite_moving2, True, False)
+        ]
 
+        # Configuração inicial do sprite
         self.sprite_atual = self.sprite_parado
         self.frame_index = 0
         self.tempo_anterior = pygame.time.get_ticks()
         self.frame_rate = 200
-        self.rect = pygame.Rect(self.x, self.y, PERSONAGEM_LARGURA, PERSONAGEM_ALTURA)
+
+        # Configuração inicial do sprite
+        self.sprite_atual = self.sprite_parado
+        self.frame_index = 0
+        self.tempo_anterior = pygame.time.get_ticks()
+        self.frame_rate = 200
 
     def atualizar(self, teclas):
-        self.moving = False
-
+        # Movimentação com as setas esquerda e direita
         if teclas[pygame.K_LEFT]:
             self.x -= self.velocidade
             self.direcao = 'esquerda'
@@ -39,23 +42,23 @@ class Personagem:
             self.x += self.velocidade
             self.direcao = 'direita'
             self.moving = True
+        else:
+            self.moving = False
 
-        self.x = max(0, min(self.x, LARGURA - PERSONAGEM_LARGURA))
-        self.y = max(0, min(self.y, ALTURA - PERSONAGEM_ALTURA))
-
+        # Atualiza a animação do personagem
         self.animar()
-        self.rect.topleft = (self.x, self.y)
 
     def animar(self):
         agora = pygame.time.get_ticks()
         if self.moving:
             if agora - self.tempo_anterior > self.frame_rate:
                 self.tempo_anterior = agora
-                self.frame_index = (self.frame_index + 1) % len(self.sprites_direita)
-                self.sprite_atual = self.sprites_direita[self.frame_index] if self.direcao == 'direita' else \
-                                    self.sprites_esquerda[self.frame_index]
+                self.frame_index = (self.frame_index + 1) % 2
+                self.sprite_atual = self.sprites_direita[self.frame_index] if self.direcao == 'direita' else self.sprites_esquerda[self.frame_index]
         else:
             self.sprite_atual = self.sprite_parado
 
     def desenhar(self, tela):
+        # Desenhar o sprite atual do personagem na tela
         tela.blit(self.sprite_atual, (self.x, self.y))
+
